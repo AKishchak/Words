e923639d52596b6b31ae53582c98b6354e3bb60b
Simon <simon.ternoir@unitag.io>
2016-03-31 18:07:47 +0200

    add website

---
+++ dist/build.js
@@ -0,0 +1,19 @@
+yaml = require('js-yaml');
+fs   = require('fs');
+
+var files = [
+  'vocabulary',
+  'adjectives',
+];
+
+var json = {};
+
+for (var i = 0; i < files.length; i++) {
+  try {
+    json[files[i]] = (yaml.safeLoad(fs.readFileSync('sources/' + files[i] + '.yaml', 'utf8')));
+  } catch (e) {
+    console.log(ERROR, e);
+  }
+}
+
+fs.writeFileSync('dist/words.json', JSON.stringify(json), 'utf8');
---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var russianApp = angular.module('russian', []);
+
+russianApp.controller('mainCtrl', ['$scope', '$http', function scope($scope, $http) {
+  $scope.json = {};
+
+  $http.get('/words.json').then(function callback(response) {
+    $scope.json = response.data;
+  });
+
+}]);
