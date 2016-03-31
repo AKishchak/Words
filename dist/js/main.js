615d5537cc7c291161e8caac91d4ad9377c770c9
Simon <simon.ternoir@unitag.io>
2016-03-31 19:19:12 +0200

    add dynamic menu and hidden fields

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,10 @@ var russianApp = angular.module('russian', []);
 
 russianApp.controller('mainCtrl', ['$scope', '$http', function scope($scope, $http) {
   $scope.json = {};
+  $scope.show = {
+    french: true,
+    russian: true
+  };
 
   $http.get('./words.json').then(function callback(response) {
     $scope.json = response.data;
