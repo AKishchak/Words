5df93e70555259dabb66f43bf8a7f5288cc74b85
Simon <simon.ternoir@unitag.io>
2016-04-01 22:37:52 +0200

    add shuffling

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,14 +10,38 @@ russianApp.controller('mainCtrl', ['$scope', '$http', function scope($scope, $ht
   };
 
   $http.get('./words.json').then(function callback(response) {
-    $scope.json = response.data;
+
+    for (var cat in response.data) {
+      $scope.json[cat] = [];
+      for (var word in response.data[cat]) { // eslint-disable-line one-var
+        $scope.json[cat].push({fr: word, ru: response.data[cat][word]});
+      }
+    }
   });
+
+
+  $scope.shuffling = function shuffling() {
+    var shuffledArray = [];
+    for (var cat in $scope.json) {
+      var a = $scope.json[cat];
+      var j, x, i; // eslint-disable-line one-var
+      for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+      }
+      shuffledArray = shuffledArray.concat(a);
+    }
+
+    $scope.json = {'Every day I\'m shuffling': shuffledArray};
+  };
 
   $scope.accentuate = function accentuate(str) {
     if (str) {
       str = str.replace(/\(/g, '<b>');
       str = str.replace(/\)/g, '</b>');
-      return str;
+      return '<span>' + str + '</span>';
     } else{
       return '-';
     }
